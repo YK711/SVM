ccd42d917928db8997a8fd0fbc00e9e5e49075db
YK711 <44077579+YK711@users.noreply.github.com>
2021-08-16 18:58:10 +0800

    Delete Ori-SVM directory

--- Ori-SVM/Ori_solveommiga0.m
+++
@@ -1,7 +0,0 @@
-function [ ommiga0 ] = Ori_solveommiga0( solveX,posx,posn,negx,negn,data,sigma )
-%���ommiga0
-
-possum = Ori_ommigaxi(solveX,data,posx,posn,sigma);   %��һ���<ommiga,x(i)>֮��
-negsum = Ori_ommigaxi(solveX,data,negx,negn,sigma);   %��һ���<ommiga,x(i)>֮��
-ommiga0 = -(possum+negsum-posn+negn)/(posn+negn);           %���ommiga0
-end
